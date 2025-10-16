62b5513c934e6e2c33dd62bde5ca4c68fc1f21c3
Yuvraj <yuvisingh@1810@gmail.com>
2025-10-16 17:34:39 +0530

    made the page async

--- synapse-ai-frontend/app/doc/[id]/page.tsx
+++ synapse-ai-frontend/app/doc/[id]/page.tsx
@@ -2,14 +2,17 @@
 
 import Document from "@/components/Document";
 
-export default function DocumentPage({
+export default async function DocumentPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  const { id } = await params; //await it, since Next marks it as Promise as the Layout is async.
+
   return (
     <div className="flex flex-col flex-1 min-h-screen bg-zinc-200 dark:bg-zinc-900">
-      <Document id={params.id} />
+      <Document id={id} />
     </div>
   );
 }
+

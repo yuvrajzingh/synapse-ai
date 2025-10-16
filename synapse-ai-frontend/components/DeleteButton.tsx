c2de60a3257f234ae6246c0bde7c65c5c7e57031
Yuvraj <yuvisingh@1810@gmail.com>
2025-10-16 17:40:55 +0530

    removed useless import

--- synapse-ai-frontend/components/DeleteButton.tsx
+++ synapse-ai-frontend/components/DeleteButton.tsx
@@ -1,7 +1,7 @@
 import { Trash2Icon, TrashIcon } from 'lucide-react';
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from "../ui/button";
+
 
 const DeleteButton = () => {
   return (
